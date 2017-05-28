4177bbe65766dfdc44492601d1df5a81bbaa6ba0
Edgar <bslugger@gmail.com>
2017-05-27 17:41:35 -0700

    econizen with xpath project started.

---
+++ econizen/econizen/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  econizen
+//
+//  Created by Edgar on 3/22/17.
+//  Copyright © 2017 econizen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
